53bf5eca4215d489742022e25e879cfc95d43806
the-1aw <bguilliod@gmail.com>
2025-11-22 15:01:11 +0100

    feat(server): add count metrics middleware

--- server/server.go
+++ server/server.go
@@ -1,10 +1,48 @@
 package server
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			cfg.fileserverHits.Add(1)
+			next.ServeHTTP(w, r)
+		},
+	)
+}
+
+func (cfg *apiConfig) requestCount(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %v", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) resetCount(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
 
 func Run() {
+	cfg := apiConfig{}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./server")))
+	mux.HandleFunc("/healthz/", healthz)
+	mux.HandleFunc("/metrics/", cfg.requestCount)
+	mux.HandleFunc("/reset/", cfg.resetCount)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
