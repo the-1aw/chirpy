158f28a97299a67ff343e76192ea00c5a31c90a6
the-1aw <bguilliod@gmail.com>
2025-12-07 12:43:52 +0100

    test(auth): add test to jwt manipalition functions

--- internal/auth/jwt_test.go
+++ internal/auth/jwt_test.go
@@ -1,11 +1,83 @@
 package auth
 
 import (
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/google/uuid"
 )
+
+const (
+	ErrorIDValidation = `ID lost in validation
+expected: %v
+got: %v
+`
+	ErrorInvalidToken = `Invalid token
+expected: %#v
+got: %#v
+`
+)
+
+func createTestHeader(headers map[string]string) http.Header {
+	header := http.Header{}
+	for key, value := range headers {
+		header.Set(key, value)
+	}
+	return header
+}
+
+func TestGetBearerToken(t *testing.T) {
+	testCases := map[string]struct {
+		header http.Header
+		valid  bool
+		token  string
+	}{
+		"base case": {
+			header: createTestHeader(map[string]string{
+				"authorization": "Bearer xxx",
+			}),
+			valid: true,
+			token: "xxx",
+		},
+		"header with case": {
+			header: createTestHeader(map[string]string{
+				"AuthoriZation": "Bearer xxx",
+			}),
+			valid: true,
+			token: "xxx",
+		},
+		"bearer with case": {
+			header: createTestHeader(map[string]string{
+				"authorization": "BeArer xxx",
+			}),
+			valid: true,
+			token: "xxx",
+		},
+		"multiple spaces": {
+			header: createTestHeader(map[string]string{
+				"authorization": "BeArer      xxx",
+			}),
+			valid: true,
+			token: "xxx",
+		},
+	}
+
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			token, err := GetBearerToken(test.header)
+			if err != nil && test.valid {
+				t.Fatalf("Extraction failed for payload %#v", test.header)
+			}
+			if !test.valid {
+				t.Fatalf("Extraction should have failed for header %#v", test.header)
+			} else if token != test.token {
+				t.Fatalf(ErrorInvalidToken, test.token, token)
+			}
+
+		})
+	}
+}
 
 const (
 	secretValid   = "validSecret"
@@ -39,13 +111,6 @@ func TestJWTSignature(t *testing.T) {
 		})
 	}
 }
-
-const (
-	ErrorIDValidation = `ID lost in validation
-expected: %v
-got: %v
-`
-)
 
 func TestJWTValidation(t *testing.T) {
 	testCases := map[string]struct {
@@ -93,7 +158,6 @@ func TestJWTValidation(t *testing.T) {
 						t.Fatalf(ErrorIDValidation, test.id, uid)
 					}
 				}
-
 			}
 		})
 	}
